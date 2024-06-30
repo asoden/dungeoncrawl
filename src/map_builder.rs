f588ff9154a99f5ccc09dba4d99205465c287cad
Andrew Soden <soden.andrew@gmail.com>
2024-06-30 16:08:24 -0500

    Finish adding prefab structure to map generation

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -1,13 +1,16 @@
 use crate::prelude::*;
 
 mod automata;
+mod drunkard;
 mod empty;
 mod rooms;
+mod prefab;
 
 use empty::EmptyArchitect;
+use prefab::apply_prefab;
 use rooms::RoomsArchitect;
-
 use self::automata::CellularAutomataArchitect;
+use self::drunkard::DrunkardsWalkArchitect;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -25,8 +28,14 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = CellularAutomataArchitect {};
-        architect.new(rng)
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect{}),
+            1 => Box::new(RoomsArchitect{}),
+            _ => Box::new(CellularAutomataArchitect{})
+        };
+        let mut mb = architect.new(rng);
+        apply_prefab(&mut mb, rng);
+        mb
     }
 
     fn fill(&mut self, tile: TileType) {
